3fa381ad2a82350cca000a9576bdd354820d5011
taufiqalfianto <91525251+taufiqalfianto@users.noreply.github.com>
2021-10-14 10:27:17 +0700

    upload

---
+++ UIStyle/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIStyle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var nama = textBox1.Text;
+            var jeniskelamin = comboBox1.Text;
+            var pendidikan = comboBox2.Text;
+            var pekerjaan = comboBox3.Text;
+
+            textBox2.Text = string.Format(nama);
+            textBox3.Text = string.Format(jeniskelamin);
+            textBox4.Text = string.Format(pendidikan);
+            textBox5.Text = string.Format(pekerjaan);
+        }
+    }
+}
